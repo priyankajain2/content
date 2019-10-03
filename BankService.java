89b2cd01a4a4c0a6ca3074a46b7eb2c0d6594357
priyankajain2 <35077188+priyankajain2@users.noreply.github.com>
2019-10-03 18:03:47 +0530

    Delete BankService.java

--- BankService.java
+++
@@ -1,7 +0,0 @@
-package com.cg.ibs.im.service;
-import com.cg.ibs.im.bean.MutualFund;
-public interface BankService {
-	public void updateMFPlans(MutualFund[] mf);
-	public void updateGoldPrice(double goldPrice);
-	public void updateSilverPrice(double silverPrice);
-}
